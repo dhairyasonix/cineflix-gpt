6fca7d5b675bbc670ee515e299068ca064d61e8f
dhairyasonix <sonidhairya160@gmail.com>
2026-06-05 03:08:17 +0530

    fix error

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -18,7 +18,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 // export const auth = getAuth();
 
 export const auth = getAuth(); // Use the initialized app for authentication
